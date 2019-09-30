7eb55965fe46b23ec2e726a64841a0a5171b9c1a
dgerard42 <daniella4422@gmail.com>
2019-09-29 23:49:51 -0700

    starting on 04, it is all a broken apart mess

---
+++ 04_dgerard.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const int BOARD_SIZE = 15;
+const int X = 1;
+const int Y = 0;
+
+string  turn_left(string karelFacing)
+{
+    if (karelFacing == "east")
+        return "north";
+    else if (karelFacing == "north")
+        return "west";
+    else if (karelFacing == "west")
+        return "south";
+    else if (karelFacing == "south")
+        return "east";
+    return "error";
+}
+
+void    display_map(int coords[], char board[][], string karelFacing)
+{
+    cout << ">>Karel is at x=" << karelColumn << " y=" << karelRow;
+    cout << " and they are facing " << karelFacing << "." << endl;
+    cout << "###############" << endl;
+    for (int row; row < BOARD_SIZE; row++;)
+    {
+        cout << "#";
+        for (int column; column < BOARD_SIZE; column++)
+        {
+            cout << 
+        }
+        cout << "#";
+    }
+    cout << "###############" << endl;
+    return;
+}
+
+void    board_init(char board[][], int coords[])
+{
+    for (int row; row < BOARD_SIZE; row++;)
+        for (int column; column < BOARD_SIZE; column++)
+            board[row][column] = ".";
+    board[coords[1]][coords[0]] = "O";
+    cout << ">>Hello, I am Karel v 0.1. Please enter a command: move();,";
+    cout << " turnLeft();, or quit" << endl;
+}
+
+int     move(int coords[])
+{
+    if (karelFacing == "east")
+        coords[X]++;
+    else if (karelFacing == "west")
+        coords[X]--;
+    else if (karelFacing == "north")
+        coords[Y]++;
+    else if (karelFacing == "south")
+        coords[Y]--;
+    if (coords[X] >= BOARD_SIZE || karelRow[Y] < 0 || karelColumn[X] >= BOARD_SIZE || karelRow[Y] < 0)
+    {
+        cout << ">>ouch. I have crashed. shutting down" << endl;
+        return 1;
+    }
+    return 0;
+}
+
+int     main()
+{
+    int     error = 0;
+    int     coords[2] = {0,0};
+    char    board[BOARD_SIZE + 1][BOARD_SIZE + 1];
+    string  karelFacing = "east";
+    string  input = "startup";
+
+    board_init(board);
+    while (karelFacing != "quit" && error == 0)
+    {
+        cin >> input;
+        if (input == "move();")
+            error = move(coords);
+        else if (input == "turnLeft();")
+            karelFacing = turnLeft(karelFacing);
+        else if (input == "quit")
+        {
+            cout << ">>goodbye friend" << endl;
+            break;
+        }
+        else
+        {
+            cout << ">>Please enter one of the valid commands, which are as";
+            cout << " follows: move();, turnLeft();, quit" << endl;
+        }
+        displayMap(karelRow, karelColumn, karelFacing);
+    }
+    return 0;
+}
