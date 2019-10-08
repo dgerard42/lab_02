efeeebfdd482a56398a22034d123a0d6961a1efc
dgerard42 <daniella4422@gmail.com>
2019-10-08 13:37:51 -0700

    02 cleaned up a bit more, 05 started writing

--- 02_dgerard.cpp
+++ 02_dgerard.cpp
@@ -6,7 +6,7 @@
 /*   By: dany <github.com/dgerard42>               |;;,      "-._             */
 /*                                                 ';;;,,    ",_ "=-._        */
 /*   Created: 2019/10/05 22:11:55 by dany            ':;;;;,,..-``"-._`"-.    */
-/*   Updated: 2019/10/07 22:52:19 by dany              _/_/`           `'"`   */
+/*   Updated: 2019/10/08 11:32:25 by dany              _/_/`           `'"`   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,14 @@ const int UPPERCASE_Z = 90;
 const int LOWERCASE_A = 97;
 const int LOWERCASE_Z = 122;
 const int VOWEL = 1;
-const int CONSONANT = 0;
+const int CONSONANT = 0;         //a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,non-letter
+const int ALPHABET[LETTERS + 1] = {1,0,0,0,1,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,0,2};
 
-int         main(){
-    string  input = "null";
+void        countVowels(string &input, int &vowelCount, int &consonantCount){
+
     int     index = 0;
-    int     vowel_count = 0;
-    int     consonant_count = 0;
-    int     ascii = 0;             //a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,non-letter
-    int     alphabet[LETTERS + 1] = {1,0,0,0,1,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,0,2};
+    int     ascii = 0;
 
-    cout << "Enter a string:" << endl;
-    cin >> input;
     while ( input[index] != '\0'){
         ascii = (int)input[index];        //convert each letter to its ascii character
         if ( ascii >= UPPERCASE_A && ascii <= UPPERCASE_Z ) //if the letter is uppercase
@@ -41,12 +37,25 @@ int         main(){
             ascii -= LOWERCASE_A;                               //convert to corresponding number 0-25
         else
             ascii = LETTERS;                                    //else assign ascii with 2, the designated non character number
-        if ( alphabet[ascii] == VOWEL )                    //if the letter is a vowel (represented by 1 in alphabet array)
-            vowel_count++;                                 //count that vowel
-        else if ( alphabet[ascii] == CONSONANT )           //else if the letter is a consonant (represented by 0 in alpha array)
-            consonant_count++;                             //count that consonant
+        if ( ALPHABET[ascii] == VOWEL )                    //if the letter is a vowel (represented by 1 in alphabet array)
+            vowelCount++;                                 //count that vowel
+        else if ( ALPHABET[ascii] == CONSONANT )           //else if the letter is a consonant (represented by 0 in alpha array)
+            consonantCount++;                             //count that consonant
         index++;
     }
-    cout << "The word " << input << " contains " << vowel_count << " vowels and " << consonant_count
-            << " consonants." << endl;
+    return;
+}
+
+int         main(){
+
+    string  input = "null";
+    int     vowelCount = 0;
+    int     consonantCount = 0;
+   
+    cout << "Enter a string:" << endl;
+    cin >> input;
+    countVowels(input, vowelCount, consonantCount);
+    cout << "The word " << input << " contains " << vowelCount << " vowels and " << consonantCount
+        << " consonants." << endl;
+    return 0;
 }
--- 05_dgerard.cpp
+++ 05_dgerard.cpp
@@ -6,8 +6,49 @@
 /*   By: dany <github.com/dgerard42>               |;;,      "-._             */
 /*                                                 ';;;,,    ",_ "=-._        */
 /*   Created: 2019/10/03 10:46:44 by dany            ':;;;;,,..-``"-._`"-.    */
-/*   Updated: 2019/10/03 10:46:45 by dany              _/_/`           `'"`   */
+/*   Updated: 2019/10/08 13:35:19 by dany              _/_/`           `'"`   */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+const int   NUM_COUNTERS = 10;
+const float RANGE_LIMITS[NUM_COUNTERS] = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
+
+void        printGraph(int counters[]){
+   
+    
+
+    for (int index = 0; index < NUM_COUNTERS; index++){
+        cout << counters[index] << endl; 
+    }
+    return;
+}
+
+void        create_randoms(int counters[]){
+    
+    int     random_num = 0;
+    float   random_dec = 0;
+    
+    srand (time(NULL));
+    for (int index = 0; index < 10000; index++){
+        random_num = rand() % 10000;
+        random_dec = (float)random_num / 10000;
+        cout << random_dec << endl;
+        random_num %= 10;
+        counters[random_num]++;
+    }
+    return;
+}
+
+int         main(){
+    
+    int     counters[10] = {0,0,0,0,0,0,0,0,0,0};
+
+    create_randoms(counters);
+    print_graph(counters);
+    return 0;
+}
