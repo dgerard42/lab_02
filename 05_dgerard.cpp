3ec9d6e94a5148597bc4896c66eac94a1590bee3
dgerard42 <daniella4422@gmail.com>
2019-10-15 13:34:31 -0700

    lab progress and some from last night

--- 05_dgerard.cpp
+++ 05_dgerard.cpp
@@ -6,7 +6,7 @@
 /*   By: dany <github.com/dgerard42>               |;;,      "-._             */
 /*                                                 ';;;,,    ",_ "=-._        */
 /*   Created: 2019/10/03 10:46:44 by dany            ':;;;;,,..-``"-._`"-.    */
-/*   Updated: 2019/10/13 21:53:03 by dany              _/_/`           `'"`   */
+/*   Updated: 2019/10/15 10:57:56 by dany              _/_/`           `'"`   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,23 @@ const int   NUM_COUNTERS = 10;
 const float RANGE_LIMITS[NUM_COUNTERS] = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
 const int   MAX_HEIGHT = 50;
 
+
 void        makeGraph(int counters[], string graph[]){
 
-    int hashtags = 0;
+    int     hashtags = 0;
+    int     index = 0;
 
-    for (int row = 0; row < NUM_COUNTERS; row++){
-        hashtags = (counters[row] % 50);
-        graph[hashtags + 1][row] = '_';
-        while (hashtags > 0) {
-            graph[MAX_HEIGHT - hashtags][row] = '#';
+    for (int row = 2; row < (NUM_COUNTERS * 5); row += 5){ //iterating through all of the positions where I want to put columns
+        hashtags = (counters[index] / MAX_HEIGHT);         //turn counters into their relative values
+        graph[(MAX_HEIGHT - hashtags) - 1][row] = '_';      //place a '_' at the top of every bar
+        cout << "hashtags vs. counters == " << hashtags << " vs. " << counters[index] << endl;
+        while (hashtags > 0) {                              //for each hashmark needed to make the bar
+            graph[MAX_HEIGHT - hashtags][row] = '#';        //place # in the center of the bar
+            graph[MAX_HEIGHT - hashtags][row - 1] = '|';    //place | on either side of the bar
+            graph[MAX_HEIGHT - hashtags][row + 1] = '|';
             hashtags--;
         }
+        index++;
     }
     return;
 }
@@ -37,13 +43,10 @@ void        makeGraph(int counters[], string graph[]){
 void        graphManage(string graph[], string command){
     
     for (int row = 0; row < MAX_HEIGHT; row++){
-//        cout << "HERE0" << endl;
-        for (int column = 0; column < NUM_COUNTERS + 8; column++){
             if (command == "init")
-                graph[row][column] = '.';
+                graph[row] = "..................................................";
             else if (command == "print")
-                cout << graph[row][column];
-            }
+                cout << graph[row];
         if (command == "print")
             cout << endl;
     }
@@ -56,12 +59,12 @@ void        createRandoms(int counters[]){
     float   randomDec = 0;
     
     srand (time(nullptr));
-    for (int index = 0; index < 10000; index++){
-        randomNum = rand() % 10000;
-        randomDec = (float)randomNum / 10000;
-        cout << randomDec << endl;
-        randomNum %= 10;
-        counters[randomNum]++;
+    for (int index = 0; index < 10000; index++){ //for each random number which must be generated
+        randomNum = rand() % 10000;              //generate a number between 0 & 10000
+        randomDec = (float)randomNum / 10000;   //convert that number to a decimal between 0 & 1
+        cout << randomDec << endl;              
+        randomNum %= 10;                        //take the whole number version of that decimal and make it a number between 0 and 9
+        counters[randomNum]++;                  //go the counter at that index and add one
     }
     return;
 }
@@ -73,7 +76,6 @@ int         main(){
 
     createRandoms(counters);
     graphManage(graph, "init");
-  //  cout << "HERE1" << endl;
     makeGraph(counters, graph);
     graphManage(graph, "print");
     return 0;
--- 07_dgerard.cpp
+++ 07_dgerard.cpp
@@ -6,7 +6,7 @@
 /*   By: dany <github.com/dgerard42>               |;;,      "-._             */
 /*                                                 ';;;,,    ",_ "=-._        */
 /*   Created: 2019/10/13 21:54:00 by dany            ':;;;;,,..-``"-._`"-.    */
-/*   Updated: 2019/10/13 23:29:18 by dany              _/_/`           `'"`   */
+/*   Updated: 2019/10/15 13:33:45 by dany              _/_/`           `'"`   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,17 +35,24 @@ void        oddFirst(int arr[], int arraySize){
     
 }
 
-void        reverse(int arr[], int arraySize){
-
-}
-
 */
+
+void        reverse(int arr[], int arraySize){
+    
+    int     temp = 0;
+
+    for(int index = 0; index < (arraySize / 2); index++){
+        temp = arr[index];
+        arr[index] = arr[(arraySize - 1) - index];
+        arr[(arraySize - 1) - index] = temp; 
+    }
+}
 
 int         search(int arr[], int n, int val, bool left){
 
-    int index = (left == true) ? 0 : n;
+    int index = (left == true) ? 0 : n - 1;
 
-    while ((left == true) ? (index < n) : (index > 0)){
+    while ((left == true) ? (index < n) : (index >= 0)){
         if (arr[index] == val)
             return index;
         (left == 1) ? (index++) : (index--);
@@ -80,22 +87,66 @@ void        testOddFirst(){
 
     return;
 }
+*/
+
+void        printArray(int arr[], int arraySize){
+
+    for (int index = 0; index < arraySize; index++){
+        cout << arr[index] << " ";
+    }
+    cout << endl;
+    return;
+}
 
 void        testReverse(){
     
+    string      name = "reverse()";
+    int         arr00[] = {1,4,6,5,2,7,10,4};
+    int         arr01[] = {0, -42, -985495, 1293812983, 00, 333333333};
+    int         arr02[] = {0};
+    int         arr03[] = {3, 3, 3, 3, 3, 3, 3, 3, 3};
+
+    cout << "_____ARRAY 00_____" << endl;
+    printArray(arr00, 8);
+    reverse(arr00, 8);
+    printArray(arr00, 8);    
+    cout << "_____ARRAY 01_____" << endl;
+    printArray(arr01, 6);
+    reverse(arr01, 6);
+    printArray(arr01, 6);    
+    cout << "_____ARRAY 02_____" << endl;
+    printArray(arr02, 1);
+    reverse(arr02, 1);
+    printArray(arr02, 1);   
+    cout << "_____ARRAY 03_____" << endl;
+    printArray(arr03, 9);
+    reverse(arr03, 9);
+    printArray(arr03, 9);    
+
     return;
 } 
-
-*/
 
 void        testSearch(){
 
     string      name = "search()";
     int         arr00[] = {1,4,6,5,2,7,10,4};
-//    int       arr01[] = {0};
+    int         arr01[] = {0, -42, -985495, 1293812983, 00, 333333333};
+    int         arr02[] = {0};
+    int         arr03[] = {3, 3, 3, 3, 3, 3, 3, 3, 3};
 
     (search(arr00, 7, 6, true) == 2) ? printResult(0, name, 0) : printResult(1, name, 0);
     (search(arr00, 7, 6, false) == 2) ? printResult(0, name, 1) : printResult(1, name, 1);
+    (search(arr00, 8, 8, true) == -1) ? printResult(0, name, 2) : printResult(1, name, 2);
+    (search(arr00, 2, 5, true) == -1) ? printResult(0, name, 3) : printResult(1, name, 3);
+    (search(arr00, 8, 4, true) == 1) ? printResult(0, name, 4) : printResult(1, name, 4);
+    (search(arr00, 8, 4, false) == 7) ? printResult(0, name, 5) : printResult(1, name, 5);
+    (search(arr01, 6, -42, false) == 1) ? printResult(0, name, 6) : printResult(1, name, 6);
+    (search(arr01, 5, 1293812983, false) == 3) ? printResult(0, name, 7) : printResult(1, name, 7);
+    (search(arr02, 1, 0, true) == 0) ? printResult(0, name, 8) : printResult(1, name, 8);
+    (search(arr02, 1, 0, false) == 0) ? printResult(0, name, 9) : printResult(1, name, 9);
+    (search(arr03, 9, 3, true) == 0) ? printResult(0, name, 10) : printResult(1, name, 10);
+    (search(arr03, 9, 3, false) == 8) ? printResult(0, name, 11) : printResult(1, name, 11);
+    (search(arr03, 6, 3, false) == 5) ? printResult(0, name, 12) : printResult(1, name, 12);
 
     return;
 }
@@ -103,7 +154,7 @@ void        testSearch(){
 int         main(){
     
     testSearch();
-    //testReverse();
+    testReverse();
     //testOddFirst();
     //testLocalMaxima();
     //testLongestRun();
